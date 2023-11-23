a6de034e63e2d91ed9ead19db8068aa0fdb57d77
Alexandre Rodrigues <alexandrerodrigues31@live.com>
2023-11-23 20:06:21 -0300

    Meu Projeto Pokedex

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,42 @@
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+
+}
+
+
+class PokemonDetailAbout {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    species;
+    height;
+    weight;
+    abilities;
+    gender;
+    egg_groups;
+    egg_cycle;
+
+
+}
+
+class PokemonDetailStats {
+    number;
+    hp;
+    attack;
+    defense;
+    sp_atk;
+    sp_def;
+    speed;
+    total;
+    type_def;
+    
+
+
+
+}
